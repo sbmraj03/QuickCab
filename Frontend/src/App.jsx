80f347dd4c3050a25f7e9bbe7a64a1c091276dbf
sbmraj03 <sumbamraj3@gmail.com>
2025-02-07 17:26:14 +0530

    User and Captain All Routes Completed

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -2,21 +2,50 @@ import React from 'react'
 import { Route, Routes } from 'react-router-dom'
 import Start from './pages/Start'
 import UserLogin from './pages/UserLogin'
-import UserSignUp from './pages/UserSignUp'
-import CaptainLogin from './pages/CaptainLogin'
-import CaptainSignUp from './pages/CaptainSignUp'
+import UserSignup from './pages/UserSignup'
+import Captainlogin from './pages/Captainlogin'
+import CaptainSignup from './pages/CaptainSignup'
 import Home from './pages/Home'
+import UserProtectWrapper from './pages/UserProtectWrapper'
+import UserLogout from './pages/UserLogout'
+import CaptainHome from './pages/CaptainHome'
+import CaptainProtectWrapper from './pages/CaptainProtectWrapper'
+import CaptainLogout from './pages/CaptainLogout'
 
 const App = () => {
+
   return (
-    <Routes>
-      <Route path="/" element={<Start />} />
-      <Route path="/home" element={<Home />} />
-      <Route path="/login" element={<UserLogin />} />
-      <Route path="/signup" element={<UserSignUp />} />
-      <Route path="/captain-login" element={<CaptainLogin />} />
-      <Route path="/captain-signup" element={<CaptainSignUp />} />
-    </Routes>
+    <div>
+      <Routes>
+        <Route path='/' element={<Start />} />
+        <Route path='/login' element={<UserLogin />} />
+        <Route path='/signup' element={<UserSignup />} />
+        <Route path='/captain-login' element={<Captainlogin />} />
+        <Route path='/captain-signup' element={<CaptainSignup />} />
+        <Route path='/home'
+          element={
+            <UserProtectWrapper>
+              <Home />
+            </UserProtectWrapper>
+          } />
+        <Route path='/user/logout'
+          element={<UserProtectWrapper>
+            <UserLogout />
+          </UserProtectWrapper>
+          } />
+        <Route path='/captain-home' element={
+          <CaptainProtectWrapper>
+            <CaptainHome />
+          </CaptainProtectWrapper>
+
+        } />
+        <Route path='/captain/logout' element={
+          <CaptainProtectWrapper>
+            <CaptainLogout />
+          </CaptainProtectWrapper>
+        } />
+      </Routes>
+    </div>
   )
 }
 
